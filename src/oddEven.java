6901f64f821ad45e335aab8f844f56a872759810
Dhruv <chodvadiyadhruv06@gmail.com>
2023-07-18 20:38:52 +0530

    second commit

---
+++ src/oddEven.java
@@ -0,0 +1,18 @@
+public class oddEven {
+    public static void main(String[] args) {
+        System.out.println("Print Even and Odd ");
+        System.out.print("Even = ");
+        for (int i = 1; i <= 10; i++) {
+            if(i%2==0) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println(" ");
+        System.out.print("Odd = ");
+        for (int i = 1; i <= 10; i++) {
+            if(i%2!=0) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
