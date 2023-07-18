d4bd81af8ae5360ddd9ce23ae0f708d02a0bfe86
Dhruv <chodvadiyadhruv06@gmail.com>
2023-07-18 20:53:04 +0530

    three commit

---
+++ src/AverageArray.java
@@ -0,0 +1,16 @@
+public class AverageArray {
+    public static void main(String[] args) {
+        System.out.println("Print average of array");
+
+        int[] arr={10,20,30,40,50};
+        int ans=0,len;
+
+        for (int i = 0; i < arr.length; i++) {
+            ans += arr[i];
+        }
+        len = ans/arr.length;
+        System.out.println("Sum of Array = "+ans);
+        System.out.println("Average Array = "+ len);
+
+    }
+}
