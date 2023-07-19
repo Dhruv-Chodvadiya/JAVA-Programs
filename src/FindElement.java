bbfb1a6ae86b6063fcada8010b62447e31156679
Dhruv <chodvadiyadhruv06@gmail.com>
2023-07-19 20:40:50 +0530

    six commit

---
+++ src/FindElement.java
@@ -0,0 +1,7 @@
+public class FindElement {
+    public static void main(String[] args){
+        int[] arr={1,2,3,4,5};
+        int n=2;
+        System.out.println(arr[arr.length-n]);
+    }
+}
---
+++ src/NthElement.java
@@ -0,0 +1,7 @@
+public class NthElement {
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4,5};
+        int n=2;
+        System.out.println(arr[n-1]);
+    }
+}
