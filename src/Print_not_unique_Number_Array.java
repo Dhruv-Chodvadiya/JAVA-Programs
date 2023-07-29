987df8bc5de02f55699cf315c8de0236ddbfa923
Dhruv <chodvadiyadhruv06@gmail.com>
2023-07-29 13:38:45 +0530

    Unique commit

--- src/Print_not_unique_Number_Array.java
+++ src/Print_not_unique_Number_Array.java
@@ -1,2 +1,20 @@
 public class Print_not_unique_Number_Array {
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4,5,2,3};
+        for(int i=0;i<arr.length-1;i++){
+            if(arr[i]<0){
+                continue;
+            }
+            int count=0;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[i]==arr[j]){
+                    count++;
+                    arr[j]=arr[j]*-1;
+                }
+            }
+            if(count>=1){
+                System.out.println(arr[i]);
+            }
+        }
+    }
 }
