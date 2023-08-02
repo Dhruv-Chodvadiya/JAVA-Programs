e27d07d0de2c9f83b635faa28446950828820339
Dhruv <chodvadiyadhruv06@gmail.com>
2023-08-02 20:34:23 +0530

    Recursion power commit

---
+++ src/recursion_power.java
@@ -0,0 +1,18 @@
+public class recursion_power {
+    public static void main(String[] args) {
+        int n=5,x=2;
+        int ans=power(x,n);
+        System.out.println(ans);
+    }
+    public static int power(int x,int n){
+        if(x==0){
+            return 0;
+        }
+        if(n==0){
+            return 1;
+        }
+        int xpow=power(x,n-1);
+        int power=x*xpow;
+        return power;
+    }
+}
