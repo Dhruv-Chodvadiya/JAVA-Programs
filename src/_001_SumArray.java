0b41d7acc5966a7b9af8bd60e91afb4f99930d7c
Dhruv <chodvadiyadhruv06@gmail.com>
2023-08-03 20:44:24 +0530

    001 commit

---
+++ src/_001_SumArray.java
@@ -0,0 +1,11 @@
+public class _001_SumArray {
+    public static void main(String[] args) {
+        int[] arr={10,20,30,40,50};
+        int ans=0;
+
+        for (int i = 0; i < arr.length; i++) {
+            ans += arr[i];
+        }
+        System.out.println("Sum of Array = "+ans);
+    }
+}
