a77d34d19fa386d9f04db87d1196a7ce5177b3bc
Dhruv <chodvadiyadhruv06@gmail.com>
2023-08-03 21:43:25 +0530

    008 commit

---
+++ src/_008_alternate.java
@@ -0,0 +1,8 @@
+public class _008_alternate {
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4,5};
+        for(int i=0;i<arr.length;i=i+2){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
