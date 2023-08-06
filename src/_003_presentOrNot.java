bfdc1e1e54b68262eb85d86914290ba4dc0e64e5
Dhruv <chodvadiyadhruv06@gmail.com>
2023-08-06 11:35:56 +0530

    003 commit

--- src/_003_presentOrNot.java
+++ src/_003_presentOrNot.java
@@ -1,5 +1,13 @@
 public class _003_presentOrNot {
     public static void main(String[] args) {
-
+        int[] arr={1,2,3,4,5};
+        int n=6;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==n){
+                System.out.println("Present");
+                return;
+            }
+        }
+        System.out.println("not Present");
     }
 }
