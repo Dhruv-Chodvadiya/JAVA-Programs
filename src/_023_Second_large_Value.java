11f4c84ac7e519b7d1cefa58023c3fb0167897a4
Dhruv <chodvadiyadhruv06@gmail.com>
2023-08-06 11:37:11 +0530

    023 commit

--- src/_023_Second_large_Value.java
+++ src/_023_Second_large_Value.java
@@ -1,4 +1,4 @@
-public class Second_large_Value {
+public class _023_Second_large_Value {
     public static void main(String[] args) {
         int[] arr={4,5,6,2,3,9,8};
         int max1=Integer.MIN_VALUE;
