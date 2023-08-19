32258d0256ac0b9103a1dfd814a6ede0cce1106c
Dhruv <chodvadiyadhruv06@gmail.com>
2023-08-19 22:54:04 +0530

    044 commit

---
+++ src/_044_matrix_reverse.java
@@ -0,0 +1,13 @@
+public class _044_matrix_reverse {
+    public static void main(String[] args) {
+        int[][] mat={{1,2,3},{4,5,6},{7,8,9}};
+        int row=mat.length;
+        int col=mat[0].length;
+        for(int r=row-1;r>=0;r--){
+            for(int c=col-1;c>=0;c--){
+                System.out.print(mat[r][c]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
