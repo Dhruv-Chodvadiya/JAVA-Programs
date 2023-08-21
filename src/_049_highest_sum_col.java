0c61685b0c9276fce9d8bfb9db28a0bff7502a93
Dhruv <chodvadiyadhruv06@gmail.com>
2023-08-21 19:17:21 +0530

    049 commit

---
+++ src/_049_highest_sum_col.java
@@ -0,0 +1,18 @@
+public class _049_highest_sum_col {
+    public static void main(String[] args) {
+        int[][] mat={{1,2,3},{4,5,6},{7,8,9}};
+        int row=mat.length;
+        int col=mat[0].length;
+        int max=0;
+        for(int r=0;r<col;r++){
+            int sum=0;
+            for(int c=0;c<row;c++){
+                sum+=mat[c][r];
+            }
+            if(max<sum){
+                max=sum;
+            }
+        }
+        System.out.println(max);
+    }
+}
