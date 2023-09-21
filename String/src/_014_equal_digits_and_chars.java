e330465b2cc208137228465eea72a4706c36030f
Dhruv <chodvadiyadhruv06@gmail.com>
2023-09-21 21:22:33 +0530

    014 commit

---
+++ String/src/_014_equal_digits_and_chars.java
@@ -0,0 +1,26 @@
+public interface _014_equal_digits_and_chars {
+    public static void main(String[] args) {
+        String name="Dhruv12345";
+        char[] chars=name.toCharArray();
+
+        int num=0;
+        int ch=0;
+        int i=0;
+
+        while (i<chars.length){
+            if(chars[i] >= '0' && chars[i]<'9'){
+                num++;
+                i++;
+            }else {
+                ch++;
+                i++;
+            }
+        }
+
+        if(num==ch){
+            System.out.println("Equals");
+        }else{
+            System.out.println("Not Equals");
+        }
+    }
+}
