82bbee7769a8a80c2d51aca3c209afbfc0edabd3
Dhruv <chodvadiyadhruv06@gmail.com>
2023-09-28 15:13:11 +0530

    032 commit

--- src/_032_INserrtion_short.java
+++ src/_032_INserrtion_short.java
@@ -5,7 +5,7 @@ public class _032_INserrtion_short {
         for(int i=1;i<arr.length;i++){
             int current=arr[i];
             int j=i-1;
-            while((j>=0) && (arr[j]<current)){
+            while((j>=0) && (arr[j]>current)){
                 arr[j+1]=arr[j];
                 j--;
             }
