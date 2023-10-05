a97991ccc1bb8ed2edbb3ba01015b4c8f1437d73
Dhruv <chodvadiyadhruv06@gmail.com>
2023-10-05 18:21:33 +0530

    002 commit

---
+++ Stack/src/Reverse_sentance_002.java
@@ -0,0 +1,21 @@
+import java.util.Stack;
+
+public class Reverse_sentance_002 {
+    public static void main(String[] args) {
+        String s = "Hello Good Morning";
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ' ') {
+                while (!stack.isEmpty()) {
+                    System.out.print(stack.pop());
+                }
+                System.out.print(" ");
+            } else {
+                stack.push(s.charAt(i));
+            }
+        }
+        while (!stack.isEmpty()) {
+            System.out.print(stack.pop());
+        }
+    }
+}
