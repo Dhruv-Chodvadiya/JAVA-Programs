48962797bc7735dfcf41a773b52d1f57087c1b70
Dhruv <chodvadiyadhruv06@gmail.com>
2023-10-11 18:19:21 +0530

    008 commit

---
+++ Stack/src/_008_prefix_postfix.java
@@ -0,0 +1,21 @@
+import java.util.Stack;
+
+public class _008_prefix_postfix {
+    public static void main(String[] args) {
+        String prefix = "+a*/*bcd-ef";
+        Stack<String> stack = new Stack<>();
+
+        for (int i = prefix.length() - 1; i >= 0; i--) {
+            if (prefix.charAt(i) == '+' || prefix.charAt(i) == '-' || prefix.charAt(i) == '*'
+                    || prefix.charAt(i) == '/') {
+                String first = stack.pop();
+                String second = stack.pop();
+                String inx = first + second + prefix.charAt(i);
+                stack.push(inx);
+            } else {
+                stack.push(String.valueOf(prefix.charAt(i)));
+            }
+        }
+        System.out.println(stack.pop());
+    }
+}
