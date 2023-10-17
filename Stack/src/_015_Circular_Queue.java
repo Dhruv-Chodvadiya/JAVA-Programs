5b77310c9adbfbc5dec281ecd611c0608ee8df6c
Dhruv <chodvadiyadhruv06@gmail.com>
2023-10-17 21:24:13 +0530

    015 commit

--- Stack/src/_015_Circular_Queue.java
+++ Stack/src/_015_Circular_Queue.java
@@ -1,4 +1,8 @@
+import java.util.Arrays;
+
 public class _015_Circular_Queue {
+    static int[] queue=new int[5];
+    static int rear=-1,front=-1;
     public static void main(String[] args) {
 //        add(10);
 //        add(20);
@@ -8,6 +12,7 @@ public class _015_Circular_Queue {
 //        remove();
 //        add(60);
 //        remove();
-//        System.out.println(Arrays.toString(queue));
+        System.out.println(Arrays.toString(queue));
     }
+
 }
