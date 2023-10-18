73c5ef71d8f6510fd7d64cd32c51285fb93adc7f
Dhruv <chodvadiyadhruv06@gmail.com>
2023-10-18 19:30:26 +0530

    016 commit

--- Stack/src/_016_Stack_using_2_Queues.java
+++ Stack/src/_016_Stack_using_2_Queues.java
@@ -9,6 +9,11 @@ public class _016_Stack_using_2_Queues {
         s.push(20);
         s.push(30);
 
+//        while (!s.isEmpty()){
+//            System.out.println(s.peek());
+//            s.pop();
+//        }
+
     }
     public static class Stack{
         Queue<Integer> q1 = new LinkedList<>();
@@ -25,5 +30,32 @@ public class _016_Stack_using_2_Queues {
                 q2.add(value);
             }
         }
+
+        public int pop() {
+            if (isEmpty()) {
+                System.out.println("Queue is Empty");
+                return -1;
+            }
+            int top = -1;
+            if (!q1.isEmpty()) {
+                while (!q1.isEmpty()) {
+                    top = q1.remove();
+                    if (q1.isEmpty()) {
+                        break;
+                    }
+                    q2.add(top);
+                }
+            } else {
+                while (!q2.isEmpty()) {
+                    top = q2.remove();
+                    if (q2.isEmpty()) {
+                        break;
+                    }
+                    q1.add(top);
+                }
+            }
+            return top;
+        }
+
     }
 }
