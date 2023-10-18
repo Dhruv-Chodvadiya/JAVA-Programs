1e5b100eaee25b31353fbe3b2918218550165033
Dhruv <chodvadiyadhruv06@gmail.com>
2023-10-18 19:29:45 +0530

    016 commit

--- Stack/src/_016_Stack_using_2_Queues.java
+++ Stack/src/_016_Stack_using_2_Queues.java
@@ -4,9 +4,26 @@ import java.util.Queue;
 public class _016_Stack_using_2_Queues {
     public static void main(String[] args) {
         Stack s = new Stack();
+
+        s.push(10);
+        s.push(20);
+        s.push(30);
+
     }
     public static class Stack{
         Queue<Integer> q1 = new LinkedList<>();
         Queue<Integer> q2 = new LinkedList<>();
+
+        public boolean isEmpty() {
+            return q1.isEmpty() && q2.isEmpty();
+        }
+
+        public void push(int value) {
+            if (!q1.isEmpty()) {
+                q1.add(value);
+            } else {
+                q2.add(value);
+            }
+        }
     }
 }
