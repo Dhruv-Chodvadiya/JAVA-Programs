88021b498b0364e0de1aa3e7d0d639843c32d427
Dhruv <chodvadiyadhruv06@gmail.com>
2023-10-19 20:36:05 +0530

    017 commit

--- Stack/src/_017_Queue_using_2_Stacks.java
+++ Stack/src/_017_Queue_using_2_Stacks.java
@@ -1,5 +1,11 @@
+import java.util.Stack;
+
 public class _017_Queue_using_2_Stacks {
     public static void main(String[] args) {
-
+        Queue q1 = new Queue();
+    }
+    public static class Queue {
+        static Stack<Integer> s1 = new Stack<>();
+        static Stack<Integer> s2 = new Stack<>();
     }
 }
