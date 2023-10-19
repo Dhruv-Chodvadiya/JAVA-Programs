98e9773616875fd86d21acf56e491a57293555af
Dhruv <chodvadiyadhruv06@gmail.com>
2023-10-19 20:38:07 +0530

    017 commit

--- Stack/src/_017_Queue_using_2_Stacks.java
+++ Stack/src/_017_Queue_using_2_Stacks.java
@@ -8,6 +8,8 @@ public class _017_Queue_using_2_Stacks {
         q1.add(30);
         q1.add(40);
 
+        q1.remove();
+
         q1.print();
     }
     public static class Queue {
@@ -16,6 +18,22 @@ public class _017_Queue_using_2_Stacks {
 
         public void add(int val){
             s1.add(val);
+        }
+
+        public void remove() {
+            int top=-1;
+            if (!s1.isEmpty()) {
+                while (!s1.isEmpty()) {
+                    top=s1.pop();
+                    if (s1.isEmpty()) {
+                        break;
+                    }
+                    s2.add(top);
+                }
+                while (!s2.isEmpty()){
+                    s1.push(s2.pop());
+                }
+            }
         }
 
         public void print(){
