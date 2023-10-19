6cc3aad64fc2d66ee079e871822ab3601a9d17d3
Dhruv <chodvadiyadhruv06@gmail.com>
2023-10-19 20:36:43 +0530

    017 commit

--- Stack/src/_017_Queue_using_2_Stacks.java
+++ Stack/src/_017_Queue_using_2_Stacks.java
@@ -3,9 +3,18 @@ import java.util.Stack;
 public class _017_Queue_using_2_Stacks {
     public static void main(String[] args) {
         Queue q1 = new Queue();
+        q1.add(10);
+        q1.add(20);
+        q1.add(30);
+        q1.add(40);
     }
     public static class Queue {
         static Stack<Integer> s1 = new Stack<>();
         static Stack<Integer> s2 = new Stack<>();
+
+        public void add(int val){
+            s1.add(val);
+        }
+
     }
 }
