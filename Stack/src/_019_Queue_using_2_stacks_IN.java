59e9747b9651dfbdd5cd4f2bd9225294d7e262b0
Dhruv <chodvadiyadhruv06@gmail.com>
2023-10-21 20:57:54 +0530

    019 commit

--- Stack/src/_019_Queue_using_2_stacks_IN.java
+++ Stack/src/_019_Queue_using_2_stacks_IN.java
@@ -3,9 +3,26 @@ import java.util.Stack;
 public class _019_Queue_using_2_stacks_IN {
     public static void main(String[] args) {
         Queue q = new Queue();
+        q.add(10);
+        q.add(20);
+        q.add(30);
+
     }
     public static class Queue {
         static Stack<Integer> s1 = new Stack<>();
         static Stack<Integer> s2 = new Stack<>();
+        public boolean isEmpty() {
+            return s1.isEmpty();
+        }
+
+        public void add(int value) {
+            while (!s1.empty()) {
+                s2.push(s1.pop());
+            }
+            s1.push(value);
+            while (!s2.isEmpty()) {
+                s1.push(s2.pop());
+            }
+        }
     }
 }
