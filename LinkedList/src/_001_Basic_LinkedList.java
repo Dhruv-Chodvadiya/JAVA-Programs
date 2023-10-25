3e823de8e2206a91ab1b78883d5894596b6363e6
Dhruv <chodvadiyadhruv06@gmail.com>
2023-10-25 22:58:23 +0530

    001 commit

--- LinkedList/src/_001_Basic_LinkedList.java
+++ LinkedList/src/_001_Basic_LinkedList.java
@@ -3,3 +3,87 @@ public class _001_Basic_LinkedList {
 
     }
 }
+class LinkedL{
+    Node head;
+    Node tail;
+
+    LinkedL(){
+        head = null;
+        tail = null;
+    }
+
+    public void insert(int data) {
+        Node node = new Node(data);
+        if(head == null){
+            head = node;
+            tail = node;
+        }else{
+            tail.next = node;
+            tail = tail.next;
+        }
+    }
+
+    public void update(int old, int newValue) {
+        Node temp = head;
+        while (temp != null){
+            if(temp.data == old){
+                temp.data = newValue;
+            }
+            temp = temp.next;
+        }
+    }
+
+    public void find(int value) {
+        Node temp = head;
+        while (temp != null){
+            if(temp.data == value){
+                System.out.println("present");
+                return;
+            }
+            temp = temp.next;
+        }
+        System.out.println("not present");
+    }
+
+    public void print() {
+        if(head == null){
+            System.out.println("LinkedList is empty");
+            return;
+        }
+        Node temp = head;
+        while (temp != null){
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+
+    public void delete(int data) {
+        if(head.data == data){
+            head = head.next;
+        }else {
+            Node temp = head;
+            while (temp != null){
+                if(temp.next.data == data){
+                    if(temp.next == tail){
+                        temp.next = null;
+                        tail = temp;
+                        return;
+                    }else {
+                        temp.next = temp.next.next;
+                        return;
+                    }
+                }
+                temp = temp.next;
+            }
+        }
+    }
+}
+
+class Node{
+    int data;
+    Node next;
+
+    public Node(int data){
+        this.data = data;
+    }
+}
