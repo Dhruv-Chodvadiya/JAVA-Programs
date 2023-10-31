195aa599cdc70c9fb9318598e5c6c387d086ec33
Dhruv <chodvadiyadhruv06@gmail.com>
2023-10-31 20:58:28 +0530

    004 commit

--- LinkedList/src/_004_doubly_linkedlist/doubly.java
+++ LinkedList/src/_004_doubly_linkedlist/doubly.java
@@ -28,5 +28,20 @@ public class doubly {
         newNode.next=head;
         head = newNode;
     }
+    public void addLast(String data) {
+        Node newNode = new Node(data);
+        if(head == null) {
+            head = newNode;
+            return;
+        }
+
+        Node currNode = head;
+        while(currNode.next != null) {
+            currNode = currNode.next;
+        }
+
+        currNode.next = newNode;
+    }
+
 }
 
