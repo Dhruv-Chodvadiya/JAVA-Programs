fdb54353f385d498b773a482be1b6e00b9696d97
Dhruv <chodvadiyadhruv06@gmail.com>
2023-10-31 20:57:11 +0530

    004 commit

--- LinkedList/src/_004_doubly_linkedlist/doubly.java
+++ LinkedList/src/_004_doubly_linkedlist/doubly.java
@@ -1,5 +1,20 @@
 package _004_doubly_linkedlist;
 
 public class doubly {
+    private int size;
 
+    doubly() {
+        this.size=0;
+    }
+    class Node{
+        String data;
+        Node next;
+
+        Node(String data) {
+            this.data=data;
+            this.next=null;
+            size++;
+        }
+    }
 }
+
