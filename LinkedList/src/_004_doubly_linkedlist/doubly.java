6aba391983a8d6e01b92d01f2c24197b69155039
Dhruv <chodvadiyadhruv06@gmail.com>
2023-11-01 20:19:44 +0530

    004 commit

--- LinkedList/src/_004_doubly_linkedlist/doubly.java
+++ LinkedList/src/_004_doubly_linkedlist/doubly.java
@@ -84,7 +84,21 @@ public class doubly {
         return size;
     }
     public void reverseIterate() {
+        if(head == null || head.next == null){
+            return;
+        }
+        Node prevNode = head;
+        Node currNode = head.next;
+        while(currNode != null){
+            Node nextNode = currNode.next;
+            currNode.next = prevNode;
 
+            //update
+            prevNode = currNode;
+            currNode = nextNode;
+        }
+        head.next = null;
+        head = prevNode;
     }
 }
 
