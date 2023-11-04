9cb304a8ee10f590d77522d158d4ea9a2abf8f8f
Dhruv <chodvadiyadhruv06@gmail.com>
2023-11-04 16:51:44 +0530

    factorial commit

--- Recursion/src/factorial/factorial.java
+++ Recursion/src/factorial/factorial.java
@@ -1,7 +1,16 @@
 package factorial;
 
 public class factorial {
+    static void printFactorial(int i,int multiply) {
+        if(i < 1){
+            System.out.println(multiply);
+            return;
+        }
+        printFactorial(i-1,multiply*i);
+    }
     public static void main(String[] args) {
-
+        int n=5;
+        int factorial=1;
+         printFactorial(n,factorial);
     }
 }
