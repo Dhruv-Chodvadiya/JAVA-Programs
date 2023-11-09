7a9da0795554f239a9cb0105fa64e75ae1505f73
Dhruv <chodvadiyadhruv06@gmail.com>
2023-11-09 19:07:39 +0530

    008 commit

--- LinkedList/src/_008_get_mid/mid.java
+++ LinkedList/src/_008_get_mid/mid.java
@@ -1,7 +1,59 @@
 package _008_get_mid;
-
 public class mid {
     public static void main(String[] args) {
+        LinkedL list = new LinkedL();
 
+        list.insert(10);
+        list.insert(20);
+        list.insert(30);
+        list.insert(40);
+        list.insert(50);
+        list.print();
+        list.mid();
+    }
+}
+class LinkedL {
+    Node head;
+    Node tail;
+
+    LinkedL() {
+        head = null;
+        tail = null;
+    }
+
+    public void insert(int data) {
+        Node node = new Node(data);
+        if (head == null) {
+            head = node;
+            tail = node;
+        } else {
+            tail.next = node;
+            tail = tail.next;
+        }
+    }
+
+    public void print() {
+        if (head == null) {
+            System.out.println("LinkedList is empty");
+            return;
+        }
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+
+    public void mid() {
+        Node slov = head;
+        Node fast = head;
+
+    }
+}
+class Node{
+    int data;
+    Node next;
+    public Node(int data){
+        this.data = data;
     }
 }
