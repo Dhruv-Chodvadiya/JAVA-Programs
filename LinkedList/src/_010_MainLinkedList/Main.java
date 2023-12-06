af1a0070529f89d054081bf512367daa5249c6f1
Dhruv <chodvadiyadhruv06@gmail.com>
2023-12-06 22:31:08 +0530

    010 commit

--- LinkedList/src/_010_MainLinkedList/Main.java
+++ LinkedList/src/_010_MainLinkedList/Main.java
@@ -10,9 +10,7 @@ public class Main {
         l.insert(30);
         l.insert(40);
         l.insert(50);
-        l.insert(60);
-        l.insert(70);
-        l.insert(80);
+
 
 //        l.update(40,400);
 
