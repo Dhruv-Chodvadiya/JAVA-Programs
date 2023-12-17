2146b9e5ffd919fb61ad6a603b6235f6f4ce936d
Dhruv <chodvadiyadhruv06@gmail.com>
2023-12-17 20:24:25 +0530

    LRU commit

--- LRU/src/LRUCache.java
+++ LRU/src/LRUCache.java
@@ -18,7 +18,6 @@ public class LRUCache {
 class LRU {
     HashMap<Integer, Node> map = new HashMap<>();
     DoublyLinkList ll = new DoublyLinkList();
-
     int capacity;
 
     LRU(int capacity) {
